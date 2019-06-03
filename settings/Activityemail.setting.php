b93395e7d7e47f3b28810ba6e8c6d931985ef8fb
Alice Frumin <alice@aghtrategies.com>
2019-06-03 18:29:21 -0400

    adding a settings form

---
+++ settings/Activityemail.setting.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @file
+ * Settings metadata for com.aghstrategies.activityemail.
+ * Copyright (C) 2016, AGH Strategies, LLC <info@aghstrategies.com>
+ * Licensed under the GNU Affero Public License 3.0 (see LICENSE.txt)
+ */
+return array(
+  'activityemail_setting' => array(
+    'group_name' => 'Remote ID Sync Settings',
+    'group' => 'activityemail',
+    'name' => 'activityemail_setting',
+    'type' => 'Array',
+    'default' => NULL,
+    'add' => '4.6',
+    'is_domain' => 1,
+    'is_contact' => 0,
+    'description' => 'Activity Email Settings',
+    'help_text' => 'Activity Type to send Emails for and which groups/message templates to use.',
+  ),
+);
